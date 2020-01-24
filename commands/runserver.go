14a4e1ea1ccf0b84aeb2c516736ee73f700a7afd
Christopher Lamm <chris@theantichris.com>
2020-01-24 08:42:14 -0500

    setup server with root route

---
+++ commands/runserver.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	r := mux.NewRouter()
+
+	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "Welcome to this life-changing API.")
+	})
+
+	fmt.Println("Server listening!")
+	http.ListenAndServe(":8080", r)
+}
